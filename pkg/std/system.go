9c835a93ed7f93ec6a5808e1a635cfa58d05e03c
Theo Paris <theoparisdesigns@gmail.com>
2022-01-25 02:22:33 -0800

    feat: basic command exec api

--- pkg/std/system.go
+++ pkg/std/system.go
@@ -1,6 +1,7 @@
 package std
 
 import (
+	"os/exec"
 	"syscall"
 
 	"github.com/creepinson/jsos/pkg/core/require"
@@ -29,5 +30,28 @@ func SystemModule() require.ModuleLoader {
 	return func(runtime *goja.Runtime, module *goja.Object) {
 		ex := module.Get("exports").(*goja.Object)
 		ex.Set("mount", sysMount)
+		ex.Set("exec", func(call goja.FunctionCall) goja.Value {
+			if len(call.Arguments) < 1 {
+				return goja.Undefined()
+			}
+
+			cmd := call.Arguments[0].String()
+			args := []string{}
+
+			for _, arg := range call.Arguments[1:] {
+				args = append(args, arg.String())
+			}
+
+			command := exec.Command(cmd, args...)
+
+			// return stdout, stderr
+			stdout, err := command.Output()
+
+			if err != nil {
+				panic(err)
+			}
+
+			return runtime.ToValue(string(stdout))
+		})
 	}
 }
--- src/system/init.js
+++ src/system/init.js
@@ -1,10 +1,7 @@
-const { mount } = require("std/sys");
-
+const { exec } = require("std/sys");
 console.log("Booting up...");
-mount("proc", "proc", "/proc");
-mount("sysfs", "sysfs", "/sys");
-mount("tmpfs", "tmpfs", "/tmp");
 
 setInterval(() => {
+    // TODO: stdin
     console.log("Hello world");
 }, 5000);
